e368c5d9ad7044e3af67e766531b3f21c0b5ec39
Joaquin Alvarez <joaquinbrialva@gmail.com>
2023-03-04 12:33:55 -0300

    added MONGODB_URI to database config

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Joaquinbrialva:lZxWpC2SXrbjU0sx@alvarez.kblpnkd.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to technical test database"))
     .catch((error) => console.log(error))
